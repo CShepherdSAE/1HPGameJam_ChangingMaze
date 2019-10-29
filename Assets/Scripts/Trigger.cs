4da3faa9a686074d5f6e0e543387b4833c9472a6
Thomas Luckman <Tom.j.luckman@gmail.com>
2019-10-29 15:53:40 +1100

    Script and Test Scene

--- Assets/Scripts/Trigger.cs
+++ Assets/Scripts/Trigger.cs
@@ -4,26 +4,65 @@ using UnityEngine;
 
 public class Trigger : MonoBehaviour
 {
-
+    public int numOfDisappearWalls;
+    public int numOfAppearWalls;
+    public bool hasHappened = false;
     public GameObject triggerEffect;
-    public GameObject wallDisappear;
     public GameObject wallAppear;
+    public GameObject wallAppear1;
+    public GameObject wallAppear2;
+    public GameObject wallDisappear;
+    public GameObject wallDisappear1;
+    public GameObject wallDisappear2;
     public GameObject player;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 
-    private void OnTriggerEnter(Collider player)
+    public void OnTriggerEnter(Collider player)
     {
-        
+        if (hasHappened == false)
+        {
+            switch (numOfAppearWalls)
+            {
+                case 1:
+                    wallAppear.SetActive(true);
+                    break;
+                case 2:
+                    wallAppear.SetActive(true);
+                    wallAppear1.SetActive(true);
+                    break;
+                case 3:
+                    wallAppear.SetActive(true);
+                    wallAppear1.SetActive(true);
+                    wallAppear2.SetActive(true);
+                    break;
+            }
+            switch (numOfDisappearWalls)
+            {
+                case 1:
+                    wallDisappear.SetActive(false);
+                    break;
+                case 2:
+                    wallDisappear.SetActive(false);
+                    wallDisappear1.SetActive(false);
+                    break;
+                case 3:
+                    wallDisappear.SetActive(false);
+                    wallDisappear1.SetActive(false);
+                    wallDisappear2.SetActive(false);
+                    break;
+            }
+            hasHappened = true;
+        }
     }
 }
