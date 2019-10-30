4ed87f07bda6a3f76b0f691e6fdfca9c4d35977e
CShepherdSAE <44623939+CShepherdSAE@users.noreply.github.com>
2019-10-30 13:31:10 +1100

    Remade map

--- Assets/Scripts/MoveWall.cs
+++ Assets/Scripts/MoveWall.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class MoveWall : MonoBehaviour
 {
+    private bool beginscene;
+    private float timePlayed;
     public AudioSource Wallchange;
     // Start is called before the first frame update
     void Start()
@@ -14,13 +16,25 @@ public class MoveWall : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        timePlayed += Time.deltaTime;
+        if (timePlayed <= 2)
+        {
+            beginscene = true;
+
+        }
+        else
+        {
+            beginscene = false;
+        }
     }
 
     private void OnEnable()
     {
         Wallchange = GetComponent<AudioSource>();
-        StartCoroutine(Waiter());
+        if (beginscene == false)
+        {
+            StartCoroutine(Waiter());
+        }
     }
 
     //stop being bad
@@ -29,5 +43,11 @@ public class MoveWall : MonoBehaviour
         float wait_time = Random.Range (0, 1.0f);
         yield return new WaitForSeconds(wait_time);
         Wallchange.Play();
+    }
+    void Awake()
+    {
+        timePlayed = 0.0f;
+        beginscene = true;
+
     }
 }
